435b0b2d0e1a44113d07258f7b6dd77dcd9940c7
Andrew Pasco <apascos@gmail.com>
2025-05-21 14:36:35 +0100

    initial commit

---
+++ ISMM-GestureTeleop/ISMM_GestureTeleopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISMM_GestureTeleopApp.swift
+//  ISMM-GestureTeleop
+//
+//  Created by Andrew  on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ISMM_GestureTeleopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ISMM-GestureTeleopTests/ISMM_GestureTeleopTests.swift
@@ -0,0 +1,17 @@
+//
+//  ISMM_GestureTeleopTests.swift
+//  ISMM-GestureTeleopTests
+//
+//  Created by Andrew  on 21/05/2025.
+//
+
+import Testing
+@testable import ISMM_GestureTeleop
+
+struct ISMM_GestureTeleopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
